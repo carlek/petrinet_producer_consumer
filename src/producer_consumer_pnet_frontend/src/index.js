d2eef6049a491e9a470ec86dde071e0cce6af1e1
Carl Ek <carlek@m1.local>
2024-01-11 16:39:07 -0500

    Initial commit.

---
+++ src/producer_consumer_pnet_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { producer_consumer_pnet_backend } from "../../declarations/producer_consumer_pnet_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await producer_consumer_pnet_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
